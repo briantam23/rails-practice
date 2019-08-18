44ceae0f9c4d8ba6c395084a2c7c47fa1a79afc6
briantam23 <briantam23@gmail.com>
2019-08-18 19:32:32 -0400

    Refactored & added links

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,10 +1,12 @@
 class DogsController < ApplicationController
+  before_action :current_dog, only: [:show, :edit, :update, :destroy]
+
   def index
     @dogs = Dog.all
   end
 
   def show
-    @dog = Dog.find(params[:id])
+
   end
 
   def new
@@ -14,22 +16,20 @@ class DogsController < ApplicationController
   def create
     dog = Dog.create(dog_params)
 
-    redirect_to dogs_path
+    redirect_to dog_path(dog)
   end
 
   def edit
-    @dog = Dog.find(params[:id])
+
   end
 
   def update
-    @dog = Dog.find(params[:id])
     @dog.update(dog_params)
 
     redirect_to dog_path(@dog)
   end
 
   def destroy
-    @dog = Dog.find(params[:id])
     @dog.destroy
 
     redirect_to dogs_path
@@ -40,4 +40,9 @@ class DogsController < ApplicationController
   def dog_params
     params.require(:dog).permit(:name, :motto)
   end
+
+  def current_dog
+    @dog = Dog.find(params[:id])
+  end
+
 end
